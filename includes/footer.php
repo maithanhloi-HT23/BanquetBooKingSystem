8e827886a7f5201f49fc869715709ee74d813a9d
Mai Thành  Lợi <maithanhloi2001.hvact@gmail.com>
2023-11-24 08:37:04 +0700

    modified footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <!-- copyright -->
     <div class="agileits-w3layouts">
         <div class="container">
-            <p>© 2022 Online Banquet Booking System. All rights reserved | </p>
+            <p>© 2023 Online Banquet Booking System. All rights reserved | </p>
         </div>
     </div>
     <!-- //copyright -->
